934293af2ccfde40943ad893e11bfc09bb52861e
damienbod <damien_bod@hotmail.com>
2015-03-01 01:02:43 +0100

    init

---
+++ src/WebAppAngularJsHelloWorld/AngularApp/main.js
@@ -0,0 +1,31 @@
+﻿(function () {
+	var mainApp = angular.module("mainApp", ['ngRoute']);
+
+	mainApp.config( ['$routeProvider',  function ($routeProvider) {
+		$routeProvider
+			.when('/home', {
+				templateUrl: 'home.html',
+				controller: 'StudentController'
+			})
+			.when('/viewStudents', {
+				templateUrl: 'viewStudents.html',
+				controller: 'StudentController'
+			})
+			.otherwise({
+				redirectTo: '/home'
+			});
+		}]
+	);
+
+	mainApp.controller('StudentController', ['$scope', function ($scope) {
+			$scope.students = [
+				{ name: 'Mark Waugh', city: 'New York' },
+				{ name: 'Steve Jonathan', city: 'London' },
+				{ name: 'John Marcus', city: 'Paris' }
+			];
+
+			$scope.message = "Click on the hyper link to view the students list.";
+		}]
+	);
+
+})();
