9fbf6ebed2a5cb2a77084d19d61cbe36e3582287
damienbod <damien_bod@hotmail.com>
2015-09-20 17:52:20 +0200

    updating to ASP.NET 5 beta 7

--- src/WebAppAngularJsHelloWorld/Startup.cs
+++ src/WebAppAngularJsHelloWorld/Startup.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
-using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
 
 namespace WebAppAngularJsHelloWorld
 {
@@ -15,13 +10,7 @@ namespace WebAppAngularJsHelloWorld
     {
         public Startup(IHostingEnvironment env)
         {
-            // Setup configuration sources.
-            Configuration = new Configuration()
-                .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
         }
-
-        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
@@ -38,22 +27,7 @@ namespace WebAppAngularJsHelloWorld
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
         {
-            // Configure the HTTP request pipeline.
-            // Add the console logger.
             loggerfactory.AddConsole();
-
-            // Add the following to the request pipeline only in development environment.
-            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
-            {
-                app.UseBrowserLink();
-                app.UseErrorPage(ErrorPageOptions.ShowAll);
-            }
-            else
-            {
-                // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
-            }
 
             // Add static files to the request pipeline.
             app.UseStaticFiles();
